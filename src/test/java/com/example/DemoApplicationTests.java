5818423cb14b196ba7815e3af49f581cd42c68a1
linsen <1670856343@qq.com>
2019-08-16 10:21:00 +0800

    SpringBoot整合Redis

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -2,10 +2,13 @@ package com.example;
 
 import com.example.entity.User;
 import com.example.service.UserService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -14,6 +17,12 @@ public class DemoApplicationTests {
 
 	@Autowired
 	private UserService userService;
+
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+
+	@Autowired
+	private RedisTemplate redisTemplate;
 
 	@Test
 	public void contextLoads() {
@@ -24,6 +33,27 @@ public class DemoApplicationTests {
 		user.setRealName("张三");
 
 		userService.insertData(user);
+	}
+
+	@Test
+	public void testRedisForSaveString(){
+
+		//保存字符串
+		stringRedisTemplate.opsForValue().set("aaa","111");
+		System.out.println(stringRedisTemplate.opsForValue().get("aaa"));
+	}
+
+	@Test
+	public void testRedisForSaveCharacter(){
+		User user = new User();
+		user.setId(1);
+		user.setUserName("张三");
+		user.setPassWord("123");
+		user.setRealName("张三丰");
+		//保存对象
+		redisTemplate.opsForValue().set("user_1",user);
+
+		System.out.println(redisTemplate.opsForValue().get("user_1"));
 	}
 
 }
