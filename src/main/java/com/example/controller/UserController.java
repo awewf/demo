af307188d6ab281756c77d99b79ff061c4b9bd1c
linsen <1670856343@qq.com>
2019-08-16 10:19:51 +0800

    SpringBoot整合MyBatis

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.example.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.example.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +15,17 @@ public class UserController {
  
     @Autowired
     private UserService userService;
- 
+    
+
     @RequestMapping("getUser/{id}")
     public String GetUser(@PathVariable int id){
-
         return userService.Sel(id).toString();
+    }
+
+    @RequestMapping("testJson")
+    public String TestJson(@RequestBody String  json){
+        JSONObject jsonObject = JSONObject.parseObject(json);
+        String temp = jsonObject.getString("detail");
+        return temp;
     }
 }
--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -1,44 +1,18 @@
 package com.example.entity;
 
-public class User {
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class User implements Serializable {
     private Integer id;
     private String userName;
     private String passWord;
     private String realName;
+
  
-    public Integer getId() {
-        return id;
-    }
- 
-    public void setId(Integer id) {
-        this.id = id;
-    }
- 
-    public String getUserName() {
-        return userName;
-    }
- 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
- 
-    public String getPassWord() {
-        return passWord;
-    }
- 
-    public void setPassWord(String passWord) {
-        this.passWord = passWord;
-    }
- 
-    public String getRealName() {
-        return realName;
-    }
- 
-    public void setRealName(String realName) {
-        this.realName = realName;
-    }
- 
-    @Override
+    /*@Override
     public String toString() {
         return "User{" +
                 "id=" + id +
@@ -46,5 +20,5 @@ public class User {
                 ", passWord='" + passWord + '\'' +
                 ", realName='" + realName + '\'' +
                 '}';
-    }
+    }*/
 }
